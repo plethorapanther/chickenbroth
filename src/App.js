9272588d27bb841281b55d6c74741a400c7118a2
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-11-09 11:07:15 -0700

    Added functionality so that react correctly updates the checkbox status

--- src/App.js
+++ src/App.js
@@ -14,6 +14,20 @@ function App(props) {
     setTasks([...tasks, newTask]);
   }  
   
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  
   
   const taskList = tasks.map((task) => (
     <Todo
@@ -21,10 +35,13 @@ function App(props) {
       name={task.name}
       completed={task.completed}
       key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
     />
   ));
 
-  const headingText = `${taskList.length} tasks remaining`;
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
 
 
   return (
@@ -36,7 +53,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
