1174ba942fcdc88b89913ac21b29ffde7c28a394
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-11-09 11:01:24 -0700

    Updated the addTask functionality so that new tasks are create with unique ids using nanoid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
+
 
 function App(props) {
 
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name) {
-    alert(name);
-  }
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }  
   
-  const taskList = props.tasks.map((task) => (
+  
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -17,6 +23,10 @@ function App(props) {
       key={task.id}
     />
   ));
+
+  const headingText = `${taskList.length} tasks remaining`;
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
