5e0b7f0b4f123e2b028f49c5f4819a6539d09045
plethorapanther <97998646+plethorapanther@users.noreply.github.com>
2023-11-09 11:49:27 -0700

    Added accessibility functionality so that the header is selected after deleting a task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const FILTER_MAP = {
   All: () => true,
@@ -83,7 +91,15 @@ function App(props) {
   const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
 
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
 
   return (
     <div className="todoapp stack-large">
@@ -93,7 +109,10 @@ function App(props) {
         {filterList}
 
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
